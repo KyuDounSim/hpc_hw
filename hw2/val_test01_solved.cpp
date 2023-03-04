d32ffbacd0c40beb6eaf74f91a7e7a0eaf7461db
KyuDounSim <simkyudoun@gmail.com>
2023-03-04 16:33:44 -0500

    [p1]  val_test01_solved.cpp finished with the report, val_test_01_report.txt

--- hw2/val_test01_solved.cpp
+++ hw2/val_test01_solved.cpp
@@ -6,29 +6,8 @@ using namespace std;
 int main ( );
 void f ( int n );
 
-//****************************************************************************80
-
 int main ( )
 
-//****************************************************************************80
-//
-//  Purpose:
-//
-//    MAIN is the main program for TEST01.
-//
-//  Discussion:
-//
-//    TEST01 calls F, which has a memory "leak".  This memory leak can be
-//    detected by VALGRID.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    18 May 2011
-//
 {
   int n = 10;
 
@@ -38,33 +17,16 @@ int main ( )
   cout << "  A sample code for analysis by VALGRIND.\n";
 
   f ( n );
-//
-//  Terminate.
-//
+
   cout << "\n";
   cout << "TEST01\n";
   cout << "  Normal end of execution.\n";
 
   return 0;
 }
-//****************************************************************************80
 
 void f ( int n )
 
-//****************************************************************************80
-//
-//  Purpose:
-//
-//    F computes N+1 entries of the Fibonacci sequence.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    18 May 2011
-//
 {
   int i;
   int *x;
@@ -77,13 +39,17 @@ void f ( int n )
   x[1] = 1;
   cout << "  " << 1 << "  " << x[1] << "\n";
 
-  for ( i = 2; i <= n; i++ )
+  // For loop condition used  to be "i <= n", but this creates an out-of-bound
+  // memory access of the array x, should be changed to "i < n".
+  for ( i = 2; i < n; i++ )
   {
     x[i] = x[i-1] + x[i-2];
     cout << "  " << i << "  " << x[i] << "\n";
   }
 
-  delete [] x;
+  // free(x) is used instead of delete[] x. free() should be used for
+  // memory allocated using malloc
+  free(x);
 
   return;
 }
