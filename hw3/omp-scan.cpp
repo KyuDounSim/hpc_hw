574f690a6509c997bf7feb67483bd2d24f88045a
KyuDounSim <simkyudoun@gmail.com>
2023-04-02 13:50:14 -0400

    [hw3-p2] segfault error discovered.

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -29,68 +29,105 @@ void scan_omp(long* prefix_sum, const long* A, long n) {
   prefix_sum[0] = 0;
 
   long CHUNCK_SIZE {0}, END_IDX {0};
+  long* offset = (long*) malloc(p * sizeof(long));
 
   // parallel p chunks, each chunk has CHUNCK_SIZE elements
   // sequential scan on each chunk to compute the prefix sum  
-  #pragma omp parallel private(p, t, CHUNCK_SIZE) shared(prefix_sum, A, n) 
+  #pragma omp parallel default(none) private(p, t, CHUNCK_SIZE, END_IDX) shared(prefix_sum, A, n, offset) 
+  {
+    t = omp_get_thread_num();
+    p = omp_get_num_threads();
+    CHUNCK_SIZE = (n + p - 1) / p;
+    END_IDX = CHUNCK_SIZE * (t + 1);
+    // END_IDX = (CHUNCK_SIZE * (t + 1) < n) ? CHUNCK_SIZE * (t + 1) : n - 1 ;
+    // printf("CHUNCK_SIZE = %ld scan_omp: thread %d of %d\n", CHUNCK_SIZE, t, p);
+
+    for (long i = CHUNCK_SIZE * t + 1; i < END_IDX ; ++i)
+    {
+      prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+    }
+  }
+
+  #pragma omp barrier
+
+  // compute the offset for each chunk
+  #pragma omp parallel default(none) private(p, t, CHUNCK_SIZE, END_IDX) shared(prefix_sum, A, n, offset) 
+  {
+    t = omp_get_thread_num();
+    p = omp_get_num_threads();
+    CHUNCK_SIZE = (n + p - 1) / p;
+    END_IDX = CHUNCK_SIZE * (t + 1);
+    // END_IDX = (CHUNCK_SIZE * (t + 1) < n) ? CHUNCK_SIZE * (t + 1) : n - 1 ;
+    // printf("CHUNCK_SIZE = %ld scan_omp: thread %d of %d\n", CHUNCK_SIZE, t, p);
+
+    if (t == 0) {
+      offset[t] = 0;
+    } else {
+      offset[t] = prefix_sum[CHUNCK_SIZE * t - 1] + A[CHUNCK_SIZE * t - 1];
+    }
+
+    // accumulate the offset
+    #pragma omp single
+    for(int i = 1; i < p; ++i)
+    {
+      offset[i] += offset[i-1];
+    }
+    // printf("offset[%d] = %ld\n", t, offset[t]);
+  }
+
+  #pragma omp parallel default(none) private(p, t, CHUNCK_SIZE, END_IDX) shared(prefix_sum, A, n, offset) 
   {
     t = omp_get_thread_num();
     p = omp_get_num_threads();
     CHUNCK_SIZE = (n + p - 1) / p;
     END_IDX = CHUNCK_SIZE * (t + 1);
 
-    printf("CHUNCK_SIZE = %ld scan_omp: thread %d of %d\n", CHUNCK_SIZE, t, p);
-    
-    if(t == 0) {
-      printf("Under thread %d of %d indexes are \n", t, p);
-
-      for (long i = CHUNCK_SIZE * t + 1; i < END_IDX; i++)
-      {
-        printf("%ld ", i);
-      }
-      printf("\n");
-
-      // prefix_sum[i] = prefix_sum[i-1] + A[i-1];
-      // prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+    for (long i = CHUNCK_SIZE * t + 1; i < END_IDX ; ++i)
+    {
+      // printf("prefix_sum[%ld] = %ld + %ld \n", i, prefix_sum[i], offset[t]);
+      prefix_sum[i] += offset[t];
     }
   }
 
-  // // wait until all threads finish the sequential scan
-  // #pragma barrier
+  // printf("offset calculation: %ld + %ld \n", prefix_sum[END_IDX - 1], A[END_IDX - 1]);
+  // offset[t] = prefix_sum[END_IDX - 1] + A[END_IDX - 1];
+  // printf("offset[%d] = %ld\n", t, offset[t]);
 
-
-  // just to make the error 0
-  prefix_sum[0] = 0;
-  for (long i = 1; i < n; i++) {
-    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
-  }
+  // reference answer
+  // prefix_sum[0] = 0;
+  // for (long i = 1; i < n; i++) {
+  //   prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+  // }
+  free(offset);
 }
 
 int main() {
 
-  #pragma omp parallel num_threads(4)
-  {
-    printf("omp-scan from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
-  }
-  printf("\n");
-  #pragma omp parallel num_threads(8)
-  {
-    printf("omp-scan from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
-  }
-  printf("\n");
+  // #pragma omp parallel num_threads(4)
+  // {
+  //   printf("omp-scan from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+  // }
+  // printf("\n");
+  // #pragma omp parallel num_threads(8)
+  // {
+  //   printf("omp-scan from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+  // }
+  // printf("\n");
 
-  #pragma omp parallel num_threads(12)
-  {
-    printf("omp-scan from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
-  }
-  printf("\n");
+  // #pragma omp parallel num_threads(12)
+  // {
+  //   printf("omp-scan from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+  // }
+  // printf("\n");
 
-  // long N = 100000000;
-  long N = 100;
+  long N = 100000000;
+  // long N = 100;
   long* A = (long*) malloc(N * sizeof(long));
   long* B0 = (long*) malloc(N * sizeof(long));
   long* B1 = (long*) malloc(N * sizeof(long));
-  for (long i = 0; i < N; i++) A[i] = rand();
+  // for (long i = 0; i < N; i++) A[i] = rand();
+  // for simpler testing
+  for (long i = 0; i < N; i++) A[i] = i;
   for (long i = 0; i < N; i++) B1[i] = 0;
   
   double tt = omp_get_wtime();
@@ -100,6 +137,9 @@ int main() {
   tt = omp_get_wtime();
   scan_omp(B1, A, N);
   printf("parallel-scan   = %fs\n", omp_get_wtime() - tt);
+
+  // reference printing A, B0, B1
+  // for (long i = 0; i < N; ++i) printf("A[%ld] = %ld, B0[%ld] = %ld, B1[%ld] = %ld\n", i, A[i], i, B0[i], i, B1[i]);
 
   long err = 0;
   for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
