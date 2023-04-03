06858f97eb626a42ebcca5375af07302965e53df
KyuDounSim <simkyudoun@gmail.com>
2023-04-02 23:57:14 -0400

    [hw3] intermediate.

--- hw3/jacobi2D-omp.cpp
+++ hw3/jacobi2D-omp.cpp
@@ -15,14 +15,91 @@ inline omp_int_t omp_get_num_threads() { return 1;}
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char** argv) {
-  printf("maximum number of threads = %d\n", omp_get_num_threads());
+#include "utils.h"
 
-  // pragma is ignored if command is undefined
-  #pragma omp parallel
-  {
-    printf("jacobi2D from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+/* compuate global residual, assuming ghost values are updated */
+double compute_residual(double *u, double* f, int N, double invhsq, double a, double b)
+{
+  int i, j;
+  double tmp, res = 0.0;
+  double left = u[j - 1];    
+  double right = u[j + 1];
+  double up = u[j - N];   
+  double down = u[j + N];
+
+  #pragma omp parallel for reduction (+:res)
+  for (i = 1; i <= N; ++i) {
+    for (j = 1; j <= N; ++j) {
+      // tmp = ((2.0*u[i] - u[i-1] - u[i+1]) * invhsq - 1);
+      tmp = a * u[i * N + j] + b * u[i * N + j - 1] + b * u[i * N + j + 1] + b * u[(i - 1) * N + j] + b * u[(i + 1) * N + j];
+      res += (tmp * tmp);
+    }
   }
+  return sqrt(res);
+}	
+
+int main(int argc, char** argv) {
+  printf("maximum number of threads = %d\n", omp_get_max_threads());
+  int i, N, iter, max_iters;
+
+  sscanf(argv[1], "%d", &N);
+  sscanf(argv[2], "%d", &max_iters);
+
+  /* timing */
+  double t = omp_get_wtime();
+
+  /* Allocation of vectors, including left and right ghost points */
+  // double* u     = (double *) calloc(sizeof(double), (N + 2) * (N + 2));
+  // double* u_new = (double *) calloc(sizeof(double), (N + 2) * (N + 2));
+  // double* f     = (double *) calloc(sizeof(double), N * N);
+
+  double* u     = new double    [(N + 2) * (N + 2)];
+  double* u_new = new double    [(N + 2) * (N + 2)];
+  double* f     = new double    [ N      *  N     ];
+
+  for(int i = 0; i < N * N; ++i){ f[i] = 1.0; }
+
+  double h = 1.0 / (N + 1.0);
+  double hsq = h*h;
+  double invhsq = 1./hsq;
+  double a = 4./hsq, b = -1./hsq;
+
+  double res, res0, tol = 1e-5;
+
+  /* initial residual */
+  res0 = compute_residual(u, f, N, invhsq, a, b);
+  res = res0;
+  u[0] = u[N+1] = 0.0;
+  double omega = 1.0; //2./3;
+
+  for (iter = 0; iter < max_iters && res/res0 > tol; iter++) {
+
+    /* Jacobi step for all the inner points */
+    #pragma omp parallel for
+    for (i = 1; i <= N; i++){
+      u_new[i] =  u[i] + omega * 0.5 * (hsq + u[i - 1] + u[i + 1] - 2*u[i]);
+    }
+
+    /* flip pointers; that's faster than memcpy  */
+    // memcpy(u,unew,(N+2)*sizeof(double));
+    double* utemp = u;
+    u = u_new;
+    u_new = utemp;
+    if (0 == (iter % 1)) {
+      res = compute_residual(u, N, invhsq);
+      printf("Iter %d: Residual: %g\n", iter, res);
+    }
+  }
+
+  /* Clean up */
+  // free(u);
+  // free(u_new);
+  delete[] u;
+  delete[] u_new;
+
+  /* timing */
+  t = omp_get_wtime() - t;
+  printf("Time elapsed is %f.\n", t);
 
   return 0;
 }
--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -4,7 +4,7 @@
 #include <omp.h>
 // g++ -fopenmp omp-scan.cpp && ./a.out
 
-// on my personal Mac
+// for my personal Mac
 // g++-12 -fopenmp omp-scan.cpp && ./a.out
 
 // Scan A array and write result into prefix_sum array;
@@ -18,124 +18,166 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 }
 
 // void scan_omp(long* prefix_sum, const long* A, long n) {
+//   if (n == 0) return;
+//   prefix_sum[0] = 0;
 
+//   // long* offset = (long*) malloc((p_max) * sizeof(long));
 
-//   omp_set_num_threads(8);
-//   printf("scan %d\n", omp_get_num_threads());
+//   // parallel p chunks, each chunk has CHUNCK_SIZE elements
+//   // sequential scan on each chunk to compute the prefix sum  
+//   #pragma omp parallel shared(prefix_sum, A, n)
+//   {
+//     int t = omp_get_thread_num();
+//     int p = omp_get_num_threads();
+//     printf("scan_omp: thread %d of %d %\n", t, p);
+//     // printf("scan_omp: total thred num: %d\n", p);
+//     long CHUNCK_SIZE = (n + p - 1) / p;
+//     long START_IDX = CHUNCK_SIZE * t;
+//     long END_IDX = (t == p - 1) ? n - 1: CHUNCK_SIZE * (t + 1) ;
+//     prefix_sum[START_IDX] = A[START_IDX - 1];
 
-  // if (n == 0) return;
+//     // long CHUNCK_SIZE = n/p, i = t * CHUNCK_SIZE, 
+//     // j = (t == p-1)?(n-1):( t * CHUNCK_SIZE +CHUNCK_SIZE-1);
 
-  // #pragma omp parallel
-  // {
-  //   int p = omp_get_num_threads();
-  //   int t = omp_get_thread_num();
-  //   printf("scan_omp: thread %d of %d \n", t, p);
-  //   long CHUNCK_SIZE {0}, START_IDX {0}, END_IDX {0};
-  //   long* offset = (long*) malloc((p) * sizeof(long));
+//     // END_IDX = CHUNCK_SIZE * (t + 1);
+//     // END_IDX = (CHUNCK_SIZE * (t + 1) < n) ? CHUNCK_SIZE * (t + 1) : n - 1 ;
+//     // printf("CHUNCK_SIZE = %ld scan_omp: thread %d of %d\n", CHUNCK_SIZE, t, p);
 
-  //   // parallel p chunks, each chunk has CHUNCK_SIZE elements
-  //   // sequential scan on each chunk to compute the prefix sum  
-  //   #pragma omp parallel private(t, CHUNCK_SIZE, END_IDX) shared(prefix_sum, A, n, offset, p) 
-  //   {
-  //     CHUNCK_SIZE = (n + p - 1) / p;
-  //     START_IDX = (CHUNCK_SIZE * t) + 1;
-  //     END_IDX = (t == p - 1) ? n - 1: CHUNCK_SIZE * (t + 1) ;
+//     // scan_seq(&prefix_sum[START_IDX], &A[START_IDX], n);
 
-  //     // long CHUNCK_SIZE = n/p, i = t * CHUNCK_SIZE, 
-  //     // j = (t == p-1)?(n-1):( t * CHUNCK_SIZE +CHUNCK_SIZE-1);
+//     for (long i = START_IDX + 1; i <= END_IDX; ++i)
+//     {
+//       printf("prefix_sum[%ld] = %ld + %ld which gives", i, A[i], A[i-1]);
+//       prefix_sum[i] = prefix_sum[i - 1] + A[i-1];
+//       printf(" %ld \n", prefix_sum[i]);
+//     }
 
-  //     // END_IDX = CHUNCK_SIZE * (t + 1);
-  //     // END_IDX = (CHUNCK_SIZE * (t + 1) < n) ? CHUNCK_SIZE * (t + 1) : n - 1 ;
-  //     // printf("CHUNCK_SIZE = %ld scan_omp: thread %d of %d\n", CHUNCK_SIZE, t, p);
-  //     // printf("scan_omp: thread %d of %d has start idx %d, end_idx %d, \n", t, p
-  //     // , START_IDX, END_IDX);
+//     #pragma omp barrier
+//     // #pragma omp barrier
+//     // if (t == 0) {
+//     //   offset[t] = 0;
+//     // } else if (t == p - 1) {
+      
+//     // } else {
+//     //   offset[t] = prefix_sum[END_IDX] + A[END_IDX];
+//     // }
 
-  //     prefix_sum[0] = 0;
-  //     for (long i = START_IDX; i <= END_IDX; ++i)
-  //     {
-  //       prefix_sum[i] = A[i] + A[i - 1];
-  //       printf("prefix_sum[%ld] = %ld \n", i, prefix_sum[i]);
-  //     }
-  //     #pragma omp barrier
+//     // for (long i = CHUNCK_SIZE * t + 1; i <= END_IDX ; ++i)
+//     // {
+//     //   // prefix_sum[i] = prefix_sum[i] + offset[t];
+//     //   printf("final prefix_sum[%ld] = %ld + \n", i, prefix_sum[i]);
+//     // }
+//   }
+// }
 
-  //     if (t == 0) {
-  //       offset[t] = 0;
-  //     } else {
-  //       offset[t] = prefix_sum[END_IDX] + A[END_IDX];
-  //     }
-  //     #pragma omp barrier
+void scan_omp(long* prefix_sum, const long* A, long n) {
+  if (n == 0) return;
+  int p = omp_get_num_threads();
+  int t = omp_get_thread_num();
+  // Fill out parallel scan: One way to do this is array into p chunks
+  // Do a scan in parallel on each chunk, then share/compute the offset
+  // through a shared vector and update each chunk by adding the offset
+  // in parallel
+  prefix_sum[0] = 0;
+  #pragma omp parallel
+  {
+    int t = omp_get_thread_num(), p = omp_get_num_threads();
     
-  //   // #pragma omp single
-  //   // for(int i = 0; i < p; ++i)
-  //   // {
-  //   //   printf("offset[%d] = %ld\n", i, offset[i]);
-  //   // }
-  //   // accumulate the offset
-  //   #pragma omp single
-  //   offset[0] = 0;
-  //   for(int i = 1; i < p; ++i)
-  //   {
-  //     offset[i] += offset[i-1];
-  //     printf("offset[%d] = %ld\n", i, offset[i]);
-  //   }
-  //   }
-  // }
-  // }
+    // ranges
+    long CHUNCK_SIZE = (n + p - 1) / p;
+    long START_IDX = t * CHUNCK_SIZE;
+    // last thread may have more elements if n % p != 0
+    long END_IDX = (t == p - 1) ? n :(CHUNCK_SIZE * (t + 1));
+
+    // sub array prefix computation
+    long sum = 0;
+    if (START_IDX != 0) {
+      sum = A[START_IDX - 1];
+    }
+
+    for(long subArrayIdx = START_IDX + 1; subArrayIdx < END_IDX; ++subArrayIdx)
+      sum += A[subArrayIdx - 1];
+    
+    prefix_sum[START_IDX] = sum;
+
+    #pragma omp barrier
+
+    // prefix sum computation, picking the beginning of each thread
+    sum = 0;
+    for(long thread_id = 0; thread_id < t; ++thread_id)
+      sum += prefix_sum[thread_id * CHUNCK_SIZE];
+
+    #pragma omp barrier
+
+    prefix_sum[START_IDX] = sum + (START_IDX == 0 ? 0 : A[START_IDX - 1]);
+    for(long p_sum_idx  = START_IDX + 1; p_sum_idx  < END_IDX; ++p_sum_idx)
+      prefix_sum[p_sum_idx ] = prefix_sum[p_sum_idx - 1] + A[p_sum_idx - 1];
+
+  }
+}
+
 
 int main() {
-  // long N = 100000000;
-  long N = 100;
-  // long N = 11;
 
-  long* A = (long*) malloc(N * sizeof(long));
-  long* B0 = (long*) malloc(N * sizeof(long));
-  long* B1 = (long*) malloc(N * sizeof(long));
-  // for (long i = 0; i < N; i++) A[i] = rand();
-  for (long i = 0; i < N; i++) A[i] = i;
-  
-  double tt = omp_get_wtime();
-  scan_seq(B0, A, N);
-  printf("sequential-scan = %fs\n", omp_get_wtime() - tt);
+  long n_array[4] = {100000000, 200000000, 500000000, 1000000000};
+  long* A, *B0, *B1;
 
-  long err = 0;
+  double tt; long err;
+  for(int i = 0 ; i < 4; ++i) {
+    printf("N = %ld\n", n_array[i]);
+    A = (long*) malloc(n_array[i] * sizeof(long));
+    B0 = (long*) malloc(n_array[i] * sizeof(long));
+    B1 = (long*) malloc(n_array[i] * sizeof(long));
+    for (long i = 0; i < n_array[i]; i++) A[i] = rand();
+    
+    tt = omp_get_wtime();
+    scan_seq(B0, A, n_array[i]);
+    printf("sequential-scan = %fs\n", omp_get_wtime() - tt);
+    
+    err = 0;
 
-  omp_set_num_threads(4);
-  tt = omp_get_wtime();
-  scan_omp(B1, A, N);
-  printf("parallel-scan(4thrd)   = %fs\n", omp_get_wtime() - tt);
-  for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
-  printf("error = %ld\n", err);
+    omp_set_num_threads(3);
+    tt = omp_get_wtime();
+    scan_omp(B1, A, n_array[i]);
+    printf("parallel-scan(3thrd)   = %fs\n", omp_get_wtime() - tt);
+    for (long i = 0; i < n_array[i]; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+    printf("error = %ld\n", err);
 
-  // omp_set_num_threads(8);
-  // tt = omp_get_wtime();
-  // scan_omp(B1, A, N);
-  // printf("parallel-scan(8thrd)   = %fs\n", omp_get_wtime() - tt);
-  // for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
-  // printf("error = %ld\n", err);
+    omp_set_num_threads(8);
+    tt = omp_get_wtime();
+    scan_omp(B1, A, n_array[i]);
+    printf("parallel-scan(8thrd)   = %fs\n", omp_get_wtime() - tt);
+    for (long i = 0; i < n_array[i]; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+    printf("error = %ld\n", err);
 
-  // omp_set_num_threads(10);
-  // tt = omp_get_wtime();
-  // scan_omp(B1, A, N);
-  // printf("parallel-scan(10thrd)   = %fs\n", omp_get_wtime() - tt);
-  // for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
-  // printf("error = %ld\n", err);
-  
-  // omp_set_num_threads(20);
-  // tt = omp_get_wtime();
-  // scan_omp(B1, A, N);
-  // printf("parallel-scan(20thrd)   = %fs\n", omp_get_wtime() - tt);
-  // for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
-  // printf("error = %ld\n", err);
+    omp_set_num_threads(10);
+    tt = omp_get_wtime();
+    scan_omp(B1, A, n_array[i]);
+    printf("parallel-scan(10thrd)   = %fs\n", omp_get_wtime() - tt);
+    for (long i = 0; i < n_array[i]; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+    printf("error = %ld\n", err);
+    
+    omp_set_num_threads(20);
+    tt = omp_get_wtime();
+    scan_omp(B1, A, n_array[i]);
+    printf("parallel-scan(20thrd)   = %fs\n", omp_get_wtime() - tt);
+    for (long i = 0; i < n_array[i]; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+    printf("error = %ld\n", err);
 
-  // omp_set_num_threads(50);
-  // tt = omp_get_wtime();
-  // scan_omp(B1, A, N);
-  // printf("parallel-scan(50thrd)   = %fs\n", omp_get_wtime() - tt);
-  // for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
-  // printf("error = %ld\n", err);
+    omp_set_num_threads(50);
+    tt = omp_get_wtime();
+    scan_omp(B1, A, n_array[i]);
+    printf("parallel-scan(50thrd)   = %fs\n", omp_get_wtime() - tt);
+    for (long i = 0; i < n_array[i]; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+    printf("error = %ld\n", err);
 
-  // reference printing A, B0, B1
-  // for (long i = 0; i < N; ++i) printf("A[%ld] = %ld, B0[%ld] = %ld, B1[%ld] = %ld\n", i, A[i], i, B0[i], i, B1[i]);
+    // reference printing A, B0, B1
+    // for (long i = 0; i < N; ++i) printf("A[%ld] = %ld, B0[%ld] = %ld, B1[%ld] = %ld\n", i, A[i], i, B0[i], i, B1[i]);
+
+    // free(A);
+    // free(B0);
+    // free(B1);
+  }
 
   free(A);
   free(B0);
