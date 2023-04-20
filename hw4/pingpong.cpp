9e9d1ed5120a1ec8bae0dab40e06c3288bc618c2
simkyudoun@gmail.com <simkyudoun@gmail.com>
2023-04-20 16:03:49 -0400

    [hw4] pingpong send argument change.

--- hw4/pingpong.cpp
+++ hw4/pingpong.cpp
@@ -3,32 +3,45 @@
 #include <mpi.h>
 #include <iostream>
 
-double time_pingpong(int proc0, int proc1, long Nrepeat, long Nsize, MPI_Comm comm) {
-  int rank;
-  MPI_Comm_rank(comm, &rank);
+// To run this program, use the following
+// mpirun -np 2 ./pingpong 2 10
 
+double time_pingpong(int proc0, int proc1, long Nrepeat, long Nsize, MPI_Comm comm) {
+
+  int rank, numprocs, namelen;
   char* msg = (char*) malloc(Nsize);
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &numprocs);
+  MPI_Get_processor_name(processor_name, &namelen);
+  
   for (long i = 0; i < Nsize; i++) msg[i] = 42;
+
+  printf("Rank %d on %s\n", rank, processor_name);
 
   MPI_Barrier(comm);
   double tt = MPI_Wtime();
   for (long repeat  = 0; repeat < Nrepeat; repeat++) {
     MPI_Status status;
     if (repeat % 2 == 0) { // even iterations
-
-      if (rank == proc0)
+      if (rank == proc0) {
         MPI_Send(msg, Nsize, MPI_CHAR, proc1, repeat, comm);
-      else if (rank == proc1)
+      }
+      else if (rank == proc1) {
         MPI_Recv(msg, Nsize, MPI_CHAR, proc0, repeat, comm, &status);
+      } 
     }
     else { // odd iterations
-
-      if (rank == proc0)
-        MPI_Recv(msg, Nsize, MPI_CHAR, proc0, repeat, comm, &status);
-      else if (rank == proc1)
-        MPI_Send(msg, Nsize, MPI_CHAR, proc1, repeat, comm);
+      if (rank == proc0) {
+        MPI_Recv(msg, Nsize, MPI_CHAR, proc1, repeat, comm, &status);
+      }
+      else if (rank == proc1) {
+        MPI_Send(msg, Nsize, MPI_CHAR, proc0, repeat, comm);
+      }
     }
   }
+
   tt = MPI_Wtime() - tt;
 
   free(msg);
@@ -46,9 +59,12 @@ int main(int argc, char** argv) {
   int proc1 = atoi(argv[2]);
 
   int rank;
+  
   MPI_Comm comm = MPI_COMM_WORLD;
   MPI_Comm_rank(comm, &rank);
-
+  //MPI_Comm_size(comm, &numprocs);
+  //MPI_Get_processor_name(processor_name, &namelen);
+ 
   long Nrepeat = 1000;
   double tt = time_pingpong(proc0, proc1, Nrepeat, 1, comm);
   if (!rank) printf("pingpong latency: %e ms\n", tt/Nrepeat * 1000);
